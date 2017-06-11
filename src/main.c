e92f5872745a1c11568b265081eac1b969e1a54c
carlaraya <carl_angelo.araya@upd.edu.ph>
2017-06-11 16:55:25 +0800

    IRQs and the PIT

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 // Made for JamesM's tutorials 
 #include "monitor.h"
 #include "descriptor_tables.h"
+#include "timer.h"
 struct multiboot;
 
 int main(struct multiboot *mboot_ptr) {
     init_descriptor_tables();
+    asm volatile("sti"); // enable hardware interrupts
 
     int x;
     monitor_clear();
@@ -16,36 +18,7 @@ int main(struct multiboot *mboot_ptr) {
     monitor_write("\n");
     monitor_write("\n");
     asm volatile("int $0x0 ");
-    asm volatile("int $0x1 ");
-    asm volatile("int $0x2 ");
-    asm volatile("int $0x3 ");
-    asm volatile("int $0x4 ");
-    asm volatile("int $0x5 "); 
-    asm volatile("int $0x6 ");
-    asm volatile("int $0x7 ");
-    asm volatile("int $0x8 ");/*
-    asm volatile("int $0x9 ");
-    asm volatile("int $0xA ");
-    asm volatile("int $0xB ");
-    asm volatile("int $0xC ");
-    asm volatile("int $0xD ");
-    asm volatile("int $0xE ");
-    asm volatile("int $0xF ");
-    asm volatile("int $0x10");
-    asm volatile("int $0x11");
-    asm volatile("int $0x12");
-    asm volatile("int $0x13");
-    asm volatile("int $0x14");
-    asm volatile("int $0x15");
-    asm volatile("int $0x16");
-    asm volatile("int $0x17");
-    asm volatile("int $0x18");
-    asm volatile("int $0x19");
-    asm volatile("int $0x1A");
-    asm volatile("int $0x1B");
-    asm volatile("int $0x1C");
-    asm volatile("int $0x1D");
-    asm volatile("int $0x1E");
-    asm volatile("int $0x1F"); */
+    monitor_write("sldfj\n");
+    init_timer(50);
     return 0;
 }
